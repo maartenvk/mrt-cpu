4ea3dc90dae153843d3ad8f7d093693bce7d6748
Maarten van Keulen <maartenvkeulen@proton.me>
2024-10-19 11:16:17 +0200

    add tests for flags

---
+++ tests/tests_flags.rs
@@ -0,0 +1,43 @@
+#[cfg(test)]
+mod tests {
+    use mrt_cpu::flags::*;
+
+    #[test]
+    fn flags_initialized_off() {
+        let flags = FlagsRegister::new();
+
+        assert!(!flags.is_set(Flags::Zero));
+        assert!(!flags.is_set(Flags::Carry));
+        assert!(!flags.is_set(Flags::Sign));
+        assert!(!flags.is_set(Flags::Overflow));
+    }
+
+    #[test]
+    fn flags_can_set() {
+        let mut flags = FlagsRegister::new();
+        flags.set(Flags::Overflow);
+
+        assert!(flags.is_set(Flags::Overflow));
+    }
+
+    #[test]
+    fn flags_can_unset() {
+        let mut flags = FlagsRegister::new();
+        flags.set(Flags::Overflow);
+        flags.unset(Flags::Overflow);
+
+        assert!(!flags.is_set(Flags::Overflow));
+    }
+
+    #[test]
+    fn flags_to_vec() {
+        let mut flags = FlagsRegister::new();
+        flags.set(Flags::Carry);
+        flags.set(Flags::Sign);
+
+        let result = flags.get_flags();
+        assert_eq!(result.len(), 2);
+        assert!(matches!(result[0], Flags::Carry));
+        assert!(matches!(result[1], Flags::Sign));
+    }
+}
